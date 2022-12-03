40d1a9cf500f2fec632bba963084bcd1cf777498
lola martinhao <lola.martinhao@gmail.com>
2022-12-03 13:47:08 -0300

    merging master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,11 +19,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if current_user.admin?
-
       admin_root_path
 
     else
-
       apartaments_path
     end
   end
