dc799be7cb47b435cc8ecefd89aadfe6b46283b4
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-04 20:47:52 -0300

    created new view to create condo with user in same place

--- app/admin/condos.rb
+++ app/admin/condos.rb
@@ -34,21 +34,4 @@ ActiveAdmin.register Condo do
     f.actions
   end
 
-
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :address, :number, :city, :uf_id, :user_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :address, :number, :city, :uf_id, :user_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
 end
--- app/controllers/condos_controller.rb
+++ app/controllers/condos_controller.rb
@@ -1,15 +1,40 @@
 class CondosController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :new ]
+  skip_before_action :authenticate_user!, only: %i[ :new ]
 
   def index
   end
 
   def new
     @condo = Condo.new
+    @condo.user = User.new
     authorize @condo
   end
 
   def create
+    @condo = Condo.new(condo_params)
+    @condo.user.admin = true
+    authorize @condo
+    if @condo.save
+      redirect_to admin_root_path
+      flash[:notice] = "Parabéns. Seu condomínio foi cadastrado com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def condo_params
+    params.require(:condo).permit(:name,
+                                  :address,
+                                  :number,
+                                  :city,
+                                  :uf_id,
+                                  user_attributes: [:user_name,
+                                                    :full_name,
+                                                    :email,
+                                                    :password,
+                                                    :password_confirmation])
   end
 
   def pundit_policy_scoped?
