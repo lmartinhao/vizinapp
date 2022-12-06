1212de694620da4ec9959cdcea845507a32c7525
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-06 20:29:48 -0300

    created DetachmentCategory

---
+++ app/models/detachment_category.rb
@@ -0,0 +1,2 @@
+class DetachmentCategory < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_151743) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_232427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_151743) do
     t.datetime "updated_at", null: false
     t.index ["uf_id"], name: "index_condos_on_uf_id"
     t.index ["user_id"], name: "index_condos_on_user_id"
+  end
+
+  create_table "detachment_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "document_categories", force: :cascade do |t|
