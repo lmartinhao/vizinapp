4bc6b984183b9de02957eeaccfd804750f735a51
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-06 15:26:33 -0300

    insert in caledar events of the month

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,13 +1,14 @@
 class SchedulesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :clean_schedules, only: :index
 
   def index
-    # Scope your query to the dates being shown:
+    @schedules = []
+
     start_date = params.fetch(:start_date, Date.today).to_date
-
-    # For a monthly view:
-    @schedules = Schedule.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-
+    add_notes(start_date)
+    @schedules = Schedule.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    #raise
   end
 
   def show
@@ -19,6 +20,30 @@ class SchedulesController < ApplicationController
   def new
     @schedule = Schedule.new
   end
+
+  private
+
+  def add_notes(start_date)
+    @notes = Note.where(note_category_id: 1, noteDate: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+
+    @notes.each do |note|
+      Schedule.create!(
+        start_time: note.noteDate,
+        end_time: note.noteDate,
+        name: note.title,
+        kind: "Evento"
+      )
+    end
+  end
+
+  def clean_schedules
+    schedules = Schedule.all
+    schedules.each do |schedule|
+      schedule.destroy
+      #raise
+    end
+  end
+
 
   def pundit_policy_scoped?
     true
