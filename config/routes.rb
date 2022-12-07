773411d53609ea89d404c54ad531f96f29a29436
lola martinhao <lola.martinhao@gmail.com>
2022-12-07 10:04:40 -0300

    adjusting schedules controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   resources :schedules
   resources :condos, only: %i[ new create]
   resources :apartaments, only: [:index]
+  resources :detachments
+  resources :profiles
 end
