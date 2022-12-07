be1aa28674d7c9c3526e9e4b3d7c6fdda788a8e0
Mariane Teixeira <mariane.piovezan@hotmail.com>
2022-12-07 10:37:12 -0800

    profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,18 @@ class ProfilesController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    authorize @user
+  end
+
+  def edit
+  end
+
+  private
+
+  def pundit_policy_scoped?
+    true
+  end
+
+  def pundit_policy_authorized?
+    true
   end
 end
