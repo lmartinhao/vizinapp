05488bca187949b331654d038a65c60e940ac787
lola martinhao <lola.martinhao@gmail.com>
2022-12-08 08:27:04 -0300

    date validation fixed

--- app/models/apartament.rb
+++ app/models/apartament.rb
@@ -1,5 +1,4 @@
 class Apartament < ApplicationRecord
-
   belongs_to :condo
   belongs_to :user
 
@@ -8,5 +7,4 @@ class Apartament < ApplicationRecord
   scope :all_by_condo, ->(id) { where(condo_id: id) }
   scope :have_garage, -> { where(garage: true) }
   scope :has_condo, ->(id) { where(condo_id: id) }
-
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,5 +4,7 @@ class Meeting < ApplicationRecord
 
   validates :area_id, :schedule_date, presence: true
   validates :schedule_date, uniqueness: { scope: :area_id,
-    message: "Área já reservada para esta data" }
+                                          message: "já foi escolhida para esta área" }
+  validates :schedule_date, comparison: { greater_than: (Date.tomorrow + 1),
+                                          message: "precisa ser de, no mínimo, 3 dias depois de hoje" }
 end
